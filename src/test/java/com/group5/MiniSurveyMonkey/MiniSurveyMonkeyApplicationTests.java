cfcf4c21739ef9f126136cade3a3d96e4c645b00
John <johnwarde@cmail.carleton.ca>
2022-03-08 08:40:05 -0500

    first commit

---
+++ src/test/java/com/group5/MiniSurveyMonkey/MiniSurveyMonkeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group5.MiniSurveyMonkey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniSurveyMonkeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

517f01b0cf877db8c5e1c2998a5027362d685a2f
Vis Kirubakaran <72267229+vikiru@users.noreply.github.com>
2022-03-09 00:07:53 -0500

    Add SpringWebControllerTest
    - add basic test which just checks that "/" mapping returns status ok
    - will update with more tests as controller adds new mapping handling

---
+++ src/test/java/com/group5/MiniSurveyMonkey/SpringWebControllerTest.java
@@ -0,0 +1,24 @@
+package com.group5.MiniSurveyMonkey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = SpringWebController.class)
+@AutoConfigureMockMvc
+public class SpringWebControllerTest
+{
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnIndex() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk());
+    }
+}

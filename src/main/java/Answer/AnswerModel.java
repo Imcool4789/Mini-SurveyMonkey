669cd33430f58d96ee6de55aca22b1df7ec6fb31
John <johnwarde@cmail.carleton.ca>
2022-03-09 14:45:08 -0500

    Question and Answer packages partially completed

---
+++ src/main/java/Answer/AnswerModel.java
@@ -0,0 +1,62 @@
+package Answer;
+
+import Question.QuestionModel;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class AnswerModel {
+
+    @Id
+    private long id;
+    private String answer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private QuestionModel q;
+
+    public AnswerModel(){
+
+    }
+
+    public AnswerModel(Long id,String answer) {
+        this.id = id;
+        this.answer = answer;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public void setQuestion(QuestionModel q) {
+        this.q = q;
+    }
+
+    public QuestionModel getQuestion(){
+        return q;
+    }
+
+    @Override
+    public String toString() {
+        return "AnswerModel{" +
+                "id=" + id +
+                ", answer='" + answer + '\'' +
+                ", question=" + getQuestion() +
+                '}';
+    }
+}
---
+++ src/main/java/Question/MCQuestion.java
@@ -0,0 +1,32 @@
+package Question;
+
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class MCQuestion extends QuestionModel{
+
+    @ElementCollection
+    private List<String> opt = new ArrayList<>();
+
+    public MCQuestion() {
+        setType(QuestionType.MC);
+    }
+
+    public void setOpt(List<String> opt){
+        this.opt = opt;
+    }
+
+    public List<String> getOpt(){
+        return opt;
+    }
+
+    @Override
+    public String toString() {
+        return "MCQuestion{" +
+                "options=" + opt +
+                '}';
+    }
+}

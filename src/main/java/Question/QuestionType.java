9cf342b5db8e6e3f24c84fba6bb472c479d3ab67
Vis Kirubakaran <72267229+vikiru@users.noreply.github.com>
2022-03-10 12:50:02 -0500

    Update QuestionType and add Range Question

--- src/main/java/Question/QuestionType.java
+++ src/main/java/Question/QuestionType.java
@@ -3,4 +3,5 @@ package Question;
 public class QuestionType {
     public static final String MC = "Multiple Choice";
     public static final String OPEN = "Open Ended";
+    public static final String RANGE = "Number Range";
 }

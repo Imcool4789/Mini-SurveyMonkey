d739c534235f6b4a07586639144af494b8f4fe74
John <johnwarde@cmail.carleton.ca>
2022-03-25 08:20:17 -0400

    created survey test for answer

--- src/test/java/com/group5/MiniSurveyMonkey/test/SurveyTest/SurveyAnswerCreationTest.java
+++ src/test/java/com/group5/MiniSurveyMonkey/test/SurveyTest/SurveyAnswerCreationTest.java
@@ -1,2 +1,25 @@
-package com.group5.MiniSurveyMonkey.test.SurveyTest;public class SurveyAnswerCreationTest {
+package com.group5.MiniSurveyMonkey.test.SurveyTest;
+
+import com.group5.MiniSurveyMonkey.Answer.AnswerModel;
+import com.group5.MiniSurveyMonkey.Question.MCQuestion;
+import com.group5.MiniSurveyMonkey.Question.QuestionModel;
+import com.group5.MiniSurveyMonkey.Question.QuestionType;
+import com.group5.MiniSurveyMonkey.Survey.SurveyModel;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SurveyAnswerCreationTest {
+
+    @Test
+    public void TestAddAnswer(){
+        SurveyModel s = new SurveyModel();
+        QuestionModel q = new QuestionModel("MC", "Test Question");
+        AnswerModel a = new AnswerModel(1L,"test", q);
+
+        s.addQuestion(q);
+        s.addAnswer(a);
+
+        assertEquals(s.getSurveyAnswers().get(0).getAnswer(), "test");
+    }
 }

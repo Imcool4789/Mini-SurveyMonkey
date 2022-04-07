1a580ed19d728393c9d1e1e9909d9e3101c6c7c3
Kashish Saxena <55465586+Kashish-Saxena@users.noreply.github.com>
2022-04-07 16:34:21 -0400

    Added Reset Survey Functionality (#99)

--- src/main/java/com/group5/MiniSurveyMonkey/Login/SurveyorController.java
+++ src/main/java/com/group5/MiniSurveyMonkey/Login/SurveyorController.java
@@ -1,11 +1,17 @@
 package com.group5.MiniSurveyMonkey.Login;
 
+import com.group5.MiniSurveyMonkey.Answer.AnswerModel;
+import com.group5.MiniSurveyMonkey.Answer.AnswerRepository;
+import com.group5.MiniSurveyMonkey.Question.QuestionModel;
 import com.group5.MiniSurveyMonkey.Survey.SurveyModel;
 import com.group5.MiniSurveyMonkey.Survey.SurveyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class SurveyorController
@@ -15,6 +21,8 @@ public class SurveyorController
 
     @Autowired
     private UserRepository userRepository;
+
+    private AnswerRepository answerRepository;
 
     @GetMapping("/surveyorIndex")
     public String localLogin(Model model)
@@ -54,5 +62,21 @@ public class SurveyorController
         }
         model.addAttribute("survey", surveyModel);
         return "closeSurvey";
+    }
+
+    @GetMapping("/surveyorIndex/resetSurvey")
+    public String resetSurvey(Model model)
+    {
+        SurveyModel surveyModel = surveyRepository.findById(1);
+        if (surveyModel != null)
+        {
+            answerRepository.deleteAll();
+            for(QuestionModel q: surveyModel.getSurveyQuestions()){
+                q.setResponses(new ArrayList<>());
+            }
+        }
+        surveyRepository.save(surveyModel);
+        model.addAttribute("survey", surveyModel);
+        return "resetSurvey";
     }
 }

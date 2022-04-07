7bed58d671a8317cd5533038de6d3f5d17f66acd
vikiru <viskirubakaran@cmail.carleton.ca>
2022-04-07 18:08:08 -0400

    Create DBUserDetailsService.java

---
+++ src/main/java/com/group5/MiniSurveyMonkey/Login/DBUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.group5.MiniSurveyMonkey.Login;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class DBUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        LocalUser user = userRepository.findByUser(username);
+        if (user == null) throw new UsernameNotFoundException("User not found");
+
+        List<SimpleGrantedAuthority> authorities = Arrays.asList(new SimpleGrantedAuthority("USER"));
+        return new User(user.getUser(), user.getPassword(), authorities);
+    }
+}

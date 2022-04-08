bd1ae98d8ac01b5858e1948e5cef0227a4c68ee9
vikiru <viskirubakaran@cmail.carleton.ca>
2022-04-07 22:19:53 -0400

    Update SpringWebController.java

--- src/main/java/com/group5/MiniSurveyMonkey/SpringWebController.java
+++ src/main/java/com/group5/MiniSurveyMonkey/SpringWebController.java
@@ -3,6 +3,7 @@ package com.group5.MiniSurveyMonkey;
 import com.group5.MiniSurveyMonkey.Login.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,20 +17,20 @@ public class SpringWebController {
     @GetMapping("/index")
     public String returnHome(Model model) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String currentPrincipalName = authentication.getName();
-        System.out.println(authentication);
-        if (currentPrincipalName == "surveyor") return "redirect:/surveyorIndex";
-        else if (currentPrincipalName == "user") return "redirect:/userIndex";
+        if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ADMIN")))
+            return "redirect:/surveyorIndex";
+        else if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_USER")))
+            return "redirect:/userIndex";
         return "index";
     }
 
     @GetMapping("/viewSurvey")
     public String showSurvey(Model model) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String currentPrincipalName = authentication.getName();
-        System.out.println(authentication);
-        if (currentPrincipalName == "surveyor") return "redirect:/surveyorIndex/viewSurvey";
-        else if (currentPrincipalName == "user") return "redirect:/userIndex/viewSurvey";
+        if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ADMIN")))
+            return "redirect:/surveyorIndex/viewSurvey";
+        else if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_USER")))
+            return "redirect:/userIndex/viewSurvey";
         return "index";
     }
 

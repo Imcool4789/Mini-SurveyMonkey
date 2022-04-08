29e0030b34a9e6125f1e57be128ff6a01c32d1ac
Vis Kirubakaran <72267229+vikiru@users.noreply.github.com>
2022-04-07 22:40:10 -0400

    Refactor QuestionRangeTest

--- src/test/java/com/group5/MiniSurveyMonkey/test/QuestionTest/QuestionRangeTest.java
+++ src/test/java/com/group5/MiniSurveyMonkey/test/QuestionTest/QuestionRangeTest.java
@@ -1,22 +1,22 @@
 package com.group5.MiniSurveyMonkey.test.QuestionTest;
 
 import com.group5.MiniSurveyMonkey.Question.NumberRangeQuestion;
+import com.group5.MiniSurveyMonkey.Survey.SurveyModel;
 import org.junit.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class QuestionRangeTest {
 
-    public NumberRangeQuestion rangeQuestion;
-
     @Test
     public void TestAddGetQuestion() {
-        //rangeQuestion = new NumberRangeQuestion(1, 10, 5);
+        SurveyModel surveyModel = new SurveyModel();
+        NumberRangeQuestion rangeQuestion = new NumberRangeQuestion("qr", surveyModel, 1, 10);
         assertEquals(rangeQuestion.getMin(), 1);
         assertEquals(rangeQuestion.getMax(), 10);
-        assertEquals(rangeQuestion.getNum(), 5);
+        assertEquals(rangeQuestion.getNum(), 0);
 
-        NumberRangeQuestion rangeQuestionTwo = new NumberRangeQuestion();
+        NumberRangeQuestion rangeQuestionTwo = new NumberRangeQuestion("q2", surveyModel, 1, 10);
         rangeQuestionTwo.setMax(5);
         rangeQuestionTwo.setMin(1);
         rangeQuestionTwo.setNum(2);
